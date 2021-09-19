e53210a5b8d34e8ebd25c67d6da9d8922e910f30
IsuruVihan <isuruvihan@gmail.com>
2021-09-19 16:49:31 +0530

    Dashboard UI/UX done

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -6,10 +6,10 @@ const Dashboard = () => {
   return(
     <Container className='px-0 dashboard' fluid>
       <Top />
-      <Container className='px-0 py-5'>
+      <Container className='px-5 py-5' fluid>
         <Tab.Container defaultActiveKey="Alerts">
           <Row className='mx-0'>
-            <Col className='px-0' sm={2}>
+            <Col className='px-0' xxl={1} lg={2} sm={12}>
               <Nav variant="pills" className="flex-column">
                 <Nav.Item className='nav-btn'>
                   <Nav.Link eventKey="Alerts">Alerts</Nav.Link>
@@ -24,14 +24,26 @@ const Dashboard = () => {
                   <Nav.Link eventKey="History">History</Nav.Link>
                 </Nav.Item>
                 <Nav.Item className='nav-btn'>
-                  <Nav.Link eventKey="RegisterDriver">Register Driver</Nav.Link>
+                  <Nav.Link eventKey="Register">Register</Nav.Link>
                 </Nav.Item>
               </Nav>
             </Col>
-            <Col sm={9}>
+            <Col xxl={11} lg={10} sm={12}>
               <Tab.Content>
-                <Tab.Pane className='tab' eventKey="Alerts">
+                <Tab.Pane className='tab p-3' eventKey="Alerts">
                   Alerts
+                </Tab.Pane>
+                <Tab.Pane className='tab p-3' eventKey="Patients">
+                  Patients
+                </Tab.Pane>
+                <Tab.Pane className='tab p-3' eventKey="Ambulances">
+                  Ambulances
+                </Tab.Pane>
+                <Tab.Pane className='tab p-3' eventKey="History">
+                  History
+                </Tab.Pane>
+                <Tab.Pane className='tab p-3' eventKey="Register">
+                  Register
                 </Tab.Pane>
               </Tab.Content>
             </Col>
